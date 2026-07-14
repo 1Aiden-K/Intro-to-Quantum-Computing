fd5a3318f6dc93ca1c24a199699a4f3d0cd4d009
Aiden Khanna <aiden.khanna@gmail.com>
2026-07-14 17:33:27 -0500

    implemented entanglement swapping protocol

---
+++ Algorithms/entanglement_swapping.py
@@ -0,0 +1,44 @@
+#entanglement swapping protocol
+from Algorithms.utils import create_bell_pair, bell_measurement, pauli_correction
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit.visualization import circuit_drawer
+from matplotlib import pyplot as plt
+from qiskit.circuit import Qubit, Clbit
+
+def entanglement_swap(qc: QuantumCircuit,
+    alice: Qubit, ancillary: Qubit, bob: Qubit,
+    c0: Clbit, c1: Clbit):
+    """
+    Swaps entanglement from Carlos and Alice to Carlos and Bob.
+    Assumes:
+        Carlos and Alice are entangled,
+        Bob and ancillary are entangled,
+    """
+    bell_measurement(qc, alice, ancillary, c0, c1)
+    pauli_correction(qc, bob, c0, c1)
+
+#demo
+if __name__ == "__main__": 
+    #qubit labels
+    alice = QuantumRegister(1, name='alice')
+    ancillary = QuantumRegister(1, name='ancillary')
+    bob = QuantumRegister(1, name='bob')
+    carlos = QuantumRegister(1, name='carlos')
+    #for storing collapsed states of individual qubits, initializes to 0000
+    cr = ClassicalRegister(4, "c")
+
+
+    qc = QuantumCircuit(bob, ancillary, alice, carlos,
+                        cr)
+
+    #demo entanglements
+    create_bell_pair(qc, carlos, alice)
+    create_bell_pair(qc, ancillary, bob)
+
+    entanglement_swap(qc, alice, ancillary, bob, cr[1], cr[2])
+
+    qc.measure(bob, cr[0]), qc.measure(carlos, cr[3])
+
+    #draw the circuit
+    circuit_drawer(qc, output="mpl", style={"backgroundcolor": "#EEEEEE"}, plot_barriers=True)
+    plt.show()
--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 ## These are my exercises and algorithms from Hiu Yung Wong's Introduction to Quantum Computing
-### Notes and problem sets are in the "Chapters" folder, Algorithms can be found in the "Algorithms" folder.
+### Notes and problem sets are in the "Chapters" folder, Algorithms and protocols can be found in the "Algorithms" folder.
 
 Algorithms mainly use qiskit, but matrix/tensor operations with numpy are also present throughout many excercises.
 
 ---
 
-#### List of Completed Algoriths
+#### List of Completed Algoriths/Protocols
+(sorted most recent -> oldest)
+- **Entanglement Swapping**
+  <details>
+    <summary>See circuit diagram</summary>
+
+    ![](Algorithms/Circuit-Diagrams/entanglementSwapping.png)
+  </details>
+- **Quantum Teleportation**
+  <details>
+    <summary>See circuit diagram</summary>
+    
+    ![](Algorithms/Circuit-Diagrams/quantumTeleportation.png)
+  </details>
